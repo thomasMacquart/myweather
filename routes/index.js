fd0e36c858f60603fb30de12c156c0127e2937c7
Thomas Macquart <thomas.macquart@IL114.local>
2016-06-19 19:35:15 +0200

    read secret api key from config

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../conf.js');
+
+var apikey = config.apikey;
 
 /* GET home page. */
 /*router.get('/', function(req, res, next) {
@@ -11,7 +14,7 @@ router.get('/', function (req, res) {
 });
 
 router.get('/today', function (req, res) {
-  res.send('il fait beau');
+  res.send('il fait beau ' + apikey);
 });
 
 module.exports = router;
